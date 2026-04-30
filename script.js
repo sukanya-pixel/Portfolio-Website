725d7269b5019a1a0d5a6e838ed7ac9b89fd5449
Sukanya <sushreesukanya1235@gmail.com>
2026-04-30 21:36:16 +0530

    Upgraded UI to premium design

--- script.js
+++ script.js
@@ -2,8 +2,12 @@ function validateForm() {
   let email = document.querySelector("input[name='email']").value;
 
   if (!email.includes("@")) {
-    alert("Please enter a valid email");
+    alert("Enter valid email");
     return false;
   }
   return true;
+}
+
+function scrollToContact() {
+  document.getElementById("contact").scrollIntoView({ behavior: "smooth" });
 }
